fbbcd4d1da07567de65bc007349d0e9352955036
Luis Aguilar <laaz13@gmail.com>
2021-02-13 07:08:00 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA8fbZDMMukrzutR5giaDqZrXeTefqWKiQ",
-    authDomain: "customer-card-98193.firebaseapp.com",
-    projectId: "customer-card-98193",
-    storageBucket: "customer-card-98193.appspot.com",
-    messagingSenderId: "224155456062",
-    appId: "1:224155456062:web:5010f450d668e5d2a3f76f"
+    ## agregar tus credenciales ##
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
